4dabd97f38721deea0114c2b158b9f5c75a94bbc
James Irvin <12jdirvin@gmail.com>
2018-09-10 09:31:35 -0700

    Add lodash and example of posting data to json server

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
     providedIn: 'root'
@@ -7,11 +7,21 @@ import { HttpClient } from '@angular/common/http';
 export class DataService {
 
     private readonly BASE_API = '/api';
-    private readonly BOOKS_ENDPOINT = '/books';
+    private readonly BOOKS_ENDPOINT = this.BASE_API + '/books';
+
+    private jsonOptions = {
+        headers: new HttpHeaders({
+            'Content-type': 'application/json',
+        })
+    };
 
     constructor(private http: HttpClient) { }
 
     getBooks() {
-        return this.http.get(this.BASE_API + this.BOOKS_ENDPOINT);
+        return this.http.get(this.BOOKS_ENDPOINT);
+    }
+
+    addBook(title: string, author: string) {
+        return this.http.post(this.BOOKS_ENDPOINT, { book_title: title, author_name: author }, this.jsonOptions);
     }
 }
